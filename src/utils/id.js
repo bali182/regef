c2b4f352bebc7398da9903da0812d3237aa9f430
Édes Balázs <edesbalazs@gmail.com>
2017-12-05 11:06:37 +0000

    mad id function respect max_safe_integer

--- src/utils/id.js
+++ src/utils/id.js
@@ -1,6 +1,15 @@
+// This might need to be changed to a proper UUID function or the like.
+// I don't see any fatal flaws right now since we don't have concurrency.
+// Might need to be scoped for a Diagram just to make it clearer.
+
 let current = 0
 
+const MAX = Number.MAX_SAFE_INTEGER || 9007199254740991
+
 const id = () => {
+  if (current === MAX) {
+    current = 0
+  }
   current += 1
   return current
 }
